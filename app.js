d572bd3853dae0b492f1ef49281702beda3a272d
flute <552452006@qq.com>
2018-09-28 15:41:52 +0800

    cross domain

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const Image = require('./index')
 
 // 跨域处理
 const crossDomain = (req, res, next) => {
-    var allowedOrigins = ['https://ldsun.com', 'https://www.ldsun.com', 'null'];
+    /*var allowedOrigins = ['https://ldsun.com', 'https://www.ldsun.com', 'null'];
     var origin = req.headers.origin;
     if (allowedOrigins.indexOf(origin) > -1) {
         res.setHeader('Access-Control-Allow-Origin', origin);
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    }
+    }*/
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next()
 }
 
